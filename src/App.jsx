17975e2687b4e0e95ecb7e3bde8dec4ca545221c
Kubi <KubiHUN@users.noreply.github.com>
2022-01-10 10:01:19 +0100

    uc

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+const App = () => {
+  return (
+    <div>
+      <h1>
+        Hellow World
+      </h1>
+      <p id="first">First paragraph</p>
+      <p className="myp">Second paragraph</p>
+      <p className="myp">Third paragraph</p>
+      <input type="password" placeholder="akarmi"/>
+    </div>
+  )
+};
+
+export default App
